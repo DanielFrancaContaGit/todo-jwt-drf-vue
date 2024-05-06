6ab3fd2f79bcd691f35b4faa2019f6ea8787a351
Daniel Franca <daniel.pe.15y15@gmail.com>
2024-05-06 20:53:42 -0300

    feat fetch and list all todos

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,11 +1,15 @@
 <script setup lang='ts'>
+import router from '@/router';
 import { ref } from 'vue';
 import { RouterLink } from 'vue-router'
 
 const todoContent = ref('')
-const acssesToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE1MDUwODc3LCJpYXQiOjE3MTQ5NjQ0NzcsImp0aSI6IjdlMDYyZTRmMzE0MzRkODg5MWQ4M2U4NjAwNWMwNmE3IiwidXNlcl9pZCI6MX0.8nWAEG6Ww7cFGiCtzS8O5F8OhyWcqMyfsaz5pri2SaQ"
+const todoList = ref(false)
+const acssesToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE1MDk2NzAzLCJpYXQiOjE3MTUwMTAzMDMsImp0aSI6IjA0ODBiNWQzNzA2NDQ0ZTJhMjc1NDZiMjBhMTBlNThhIiwidXNlcl9pZCI6MX0.u93V80ctYF33PXzbr_Zv3hUJtQJcyHaDIxMBQvY_0AY"
 
-const url = new URL("http://127.0.0.1:8000/todo/list/")
+// const url = new URL("http://127.0.0.1:8000/todo/all/")
+const url = new URL("http://127.0.0.1:8000/todo/list/1/")
+
 
 async function getTodoList() {
   const ownerId = '1'
@@ -24,8 +28,21 @@ async function getTodoList() {
     headers
   })
   .then((response) => {
-    console.log(response.ok)
-    console.log(response.json())
+    // console.log(response.ok)
+    // console.log(response.json())
+    if(response.ok) {
+      return response.json()
+    } else {
+      return response.ok
+    }
+  })
+  .then((data) => {
+    if(data) {
+      return todoList.value = data
+
+    } else {
+      alert('erro')
+    }
   })
 
 }
@@ -41,7 +58,6 @@ getTodoList()
     </label>
     <button type="submit" class="btn bg-green-600 ml-3">Criat nova tarefa</button>
   </form>
-
   <table class="table w-1/2 my-8">
     <thead>
       <tr>
@@ -51,7 +67,24 @@ getTodoList()
         <th>Ações</th>
       </tr>
     </thead>
-    <tbody>
+    <tbody v-for="(todo, index) in todoList" :key="todo.id">
+      <tr>
+        <th>{{ index + 1 }}</th>
+        <td>{{ todo.content }}</td>
+        <td>
+          <div class="form-control ml-3">
+          <label class="cursor-pointer label">
+            <input type="checkbox" class="checkbox checkbox-success" />
+          </label>
+          </div>
+        </td>
+        <td class="flex justify-between w-52">
+          <button class="btn btn-warning">Editar</button>
+          <button class="btn btn-error">Deletar</button>
+        </td>
+      </tr>
+    </tbody>  
+    <!-- <tbody>
       <tr>
         <th>1</th>
         <td>Primeira atividade</td>
@@ -102,7 +135,7 @@ getTodoList()
       </tr>
 
 
-    </tbody>
+    </tbody> -->
   </table>
 
   <nav class="w-64 flex justify-between mt-8">
